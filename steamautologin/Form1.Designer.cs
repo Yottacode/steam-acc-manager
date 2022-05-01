90f110a0c276dd3e4eab678bb7b81d6b91fbe2d9
Cats-cant-lose <87536255+Cats-cant-lose@users.noreply.github.com>
2022-05-01 23:25:58 +0200

    Add files via upload

---
+++ steamautologin/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace steamautologin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.addnew = new System.Windows.Forms.Button();
+            this.loginbnt = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // addnew
+            // 
+            this.addnew.Location = new System.Drawing.Point(306, 12);
+            this.addnew.Name = "addnew";
+            this.addnew.Size = new System.Drawing.Size(99, 46);
+            this.addnew.TabIndex = 0;
+            this.addnew.Text = "add new  user";
+            this.addnew.UseVisualStyleBackColor = true;
+            this.addnew.Click += new System.EventHandler(this.addnew_Click);
+            // 
+            // loginbnt
+            // 
+            this.loginbnt.Location = new System.Drawing.Point(306, 58);
+            this.loginbnt.Name = "loginbnt";
+            this.loginbnt.Size = new System.Drawing.Size(99, 30);
+            this.loginbnt.TabIndex = 1;
+            this.loginbnt.Text = "login";
+            this.loginbnt.UseVisualStyleBackColor = true;
+            this.loginbnt.Click += new System.EventHandler(this.loginbnt_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(77, 64);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(223, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "password:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(223, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(77, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(223, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.ClientSize = new System.Drawing.Size(410, 108);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.loginbnt);
+            this.Controls.Add(this.addnew);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Steam alt Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addnew;
+        private System.Windows.Forms.Button loginbnt;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ steamautologin/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using Microsoft.Win32;
+
+
+namespace steamautologin
+{
+    public partial class Form1 : Form
+    {
+        static string path = @"C:\Data.txt";
+        struct user
+        {
+            public user(string user, string pass)
+            {
+                username = user;
+                password = pass;
+            }
+            public string username { get; }
+            public string password { get; }
+        }
+
+        // List of users
+        List<user> userlist = new List<user>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            CheckForIllegalCrossThreadCalls = false;
+
+            // if client does not have a datafile create a new 
+
+            if (!File.Exists(path))
+            {
+
+                StreamWriter sw = File.CreateText(path);
+                sw.Flush();
+                sw.Dispose();
+            }
+
+            // Load data to combobox and userlist
+            List<string> lines = File.ReadAllLines(path).ToList();
+
+            foreach (var line in lines)
+            {
+                string[] entries = line.Split(',');
+                user newuser = new user(entries[0], entries[1]);
+                comboBox1.Items.Add(entries[0]);
+                userlist.Add(newuser);
+            }
+
+        }
+
+        private void addnew_Click(object sender, EventArgs e)
+        {
+            // add new user to datafile 
+            List<string> lines = File.ReadAllLines(path).ToList();
+            var User = new user(textBox1.Text, textBox2.Text);
+            userlist.Add(User);
+            lines.Add(User.username + "," + User.password);
+            File.WriteAllLines(path, lines);
+
+            //refresh combobox
+            comboBox1.Items.Clear();
+            foreach (user v in userlist)
+            {
+                comboBox1.Items.Add(v.username);
+            }
+        }
+        
+        private void loginbnt_Click(object sender, EventArgs e)
+        {
+            // separate thread to not freeze the window 
+            var ds = new Thread(login) { IsBackground = true };
+            ds.Start();
+        }
+
+        void login()
+        {
+            // kill current steam process if there is one 
+            foreach (var process in Process.GetProcessesByName("steam"))
+            {
+                process.Kill();
+            }
+            // start steam with login
+            ProcessStartInfo startinfo = new ProcessStartInfo();
+
+            startinfo.FileName = (string)Registry.GetValue(@"HKEY_CURRENT_USER\SOFTWARE\Valve\Steam",
+                "SteamExe", "null");
+            startinfo.Arguments = " -login " + userlist[comboBox1.SelectedIndex].username + " " +
+                userlist[comboBox1.SelectedIndex].password;
+            Process.Start(startinfo);
+        }       
+    }
+}
