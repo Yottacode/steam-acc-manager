8da19dce6c1cbe8639712701ec40945ac3fa444d
Yotta <87536255+Cats-cant-lose@users.noreply.github.com>
2023-02-05 13:24:57 +0100

    Add files via upload

---
+++ Steam Account Manager/AddAccForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Steam_Account_Manager
+{
+    partial class AddAccountForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.OKBtn = new System.Windows.Forms.Button();
+            this.UsernameBox = new System.Windows.Forms.TextBox();
+            this.PasswordBox = new System.Windows.Forms.TextBox();
+            this.PasswordLbl = new System.Windows.Forms.Label();
+            this.UsernameLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelBtn.Location = new System.Drawing.Point(143, 58);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.CancelBtn.TabIndex = 4;
+            this.CancelBtn.Text = "Cancel";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
+            // 
+            // OKBtn
+            // 
+            this.OKBtn.Location = new System.Drawing.Point(224, 58);
+            this.OKBtn.Name = "OKBtn";
+            this.OKBtn.Size = new System.Drawing.Size(75, 23);
+            this.OKBtn.TabIndex = 3;
+            this.OKBtn.Text = "OK";
+            this.OKBtn.UseVisualStyleBackColor = true;
+            this.OKBtn.Click += new System.EventHandler(this.OKBtn_Click);
+            // 
+            // UsernameBox
+            // 
+            this.UsernameBox.Location = new System.Drawing.Point(76, 6);
+            this.UsernameBox.Name = "UsernameBox";
+            this.UsernameBox.Size = new System.Drawing.Size(223, 20);
+            this.UsernameBox.TabIndex = 1;
+            // 
+            // PasswordBox
+            // 
+            this.PasswordBox.Location = new System.Drawing.Point(76, 32);
+            this.PasswordBox.Name = "PasswordBox";
+            this.PasswordBox.Size = new System.Drawing.Size(223, 20);
+            this.PasswordBox.TabIndex = 2;
+            // 
+            // PasswordLbl
+            // 
+            this.PasswordLbl.AutoSize = true;
+            this.PasswordLbl.Location = new System.Drawing.Point(14, 35);
+            this.PasswordLbl.Name = "PasswordLbl";
+            this.PasswordLbl.Size = new System.Drawing.Size(56, 13);
+            this.PasswordLbl.TabIndex = 10;
+            this.PasswordLbl.Text = "Password:";
+            // 
+            // UsernameLbl
+            // 
+            this.UsernameLbl.AutoSize = true;
+            this.UsernameLbl.Location = new System.Drawing.Point(12, 9);
+            this.UsernameLbl.Name = "UsernameLbl";
+            this.UsernameLbl.Size = new System.Drawing.Size(58, 13);
+            this.UsernameLbl.TabIndex = 9;
+            this.UsernameLbl.Text = "Username:";
+            // 
+            // AddAccountForm
+            // 
+            this.AcceptButton = this.OKBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.CancelButton = this.CancelBtn;
+            this.ClientSize = new System.Drawing.Size(311, 88);
+            this.ControlBox = false;
+            this.Controls.Add(this.PasswordLbl);
+            this.Controls.Add(this.UsernameLbl);
+            this.Controls.Add(this.PasswordBox);
+            this.Controls.Add(this.UsernameBox);
+            this.Controls.Add(this.OKBtn);
+            this.Controls.Add(this.CancelBtn);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AddAccountForm";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add Account";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.AddAccountForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.Button OKBtn;
+        private System.Windows.Forms.TextBox UsernameBox;
+        private System.Windows.Forms.TextBox PasswordBox;
+        private System.Windows.Forms.Label PasswordLbl;
+        private System.Windows.Forms.Label UsernameLbl;
+    }
+}
---
+++ Steam Account Manager/AddAccForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Steam_Account_Manager
+{
+    public partial class AddAccountForm : Form
+    {
+
+        public string newUsername { get { return UsernameBox.Text; } }
+        public string newPassword { get { return PasswordBox.Text; } }
+
+        public AddAccountForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OKBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+
+        private void AddAccountForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CancelBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
